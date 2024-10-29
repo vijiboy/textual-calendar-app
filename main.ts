82608f751d145a6180d2bf0f3a884d5d06602cee
dv more <dv@dualtech.co.in>
2024-10-30 01:36:14 +0530

    fix: update dotenv import for deployment

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
-import "$std/dotenv/load.ts";
+import { load } from "https://deno.land/std@0.208.0/dotenv/mod.ts";
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
